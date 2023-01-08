0c65ed559309dc642f44b015a7e449bf2f575a53
tyrosine-rex <thomas.lahaie@etu.univ-lyon1.fr>
2023-01-09 00:16:14 +0100

    func init db

--- src/db/setup_database.py
+++ src/db/setup_database.py
@@ -1,6 +1,9 @@
+from os.path import exists
 from toml import load
-from pandas import read_csv, melt
-
+from pandas import read_csv
+from sqlalchemy.types import Integer, String, Numeric
+from sqlalchemy.engine import create_engine
+from sqlalchemy.schema import MetaData, Table, Column, ForeignKey
 
 CONFIG = "src/db/config.toml"
 
@@ -33,19 +36,62 @@ def load_count(df, phylo_col):
     count = count_abs.merge(count_rel, on=["OTU_ID", "SAMPLE_ID"])
     return count
 
+def init_database(db_path):
+    engine = create_engine(f"sqlite:///{db_path}")
+    meta = MetaData()
+
+    taxa = Table(
+        "Taxa", meta,
+        Column("OTU_ID", Integer, primary_key=True), 
+        Column("reign", String(64)), 
+        Column("phylum", String(64)), 
+        Column("class", String(64)), 
+        Column("order", String(64)), 
+        Column("family", String(64)), 
+        Column("genus", String(64)), 
+        Column("specie", String(64))
+    )
+
+    sample = Table(
+        "Sample", meta,
+        Column("SAMPLE_ID", String(64), primary_key=True), 
+        Column("gender", String(64)), 
+        Column("building", String(64)), 
+        Column("floor", String(64)), 
+        Column("surface", String(64))
+    )
+
+    count  = Table(
+        "Count", meta,
+        Column('OTU_ID', Integer, ForeignKey("Taxa.OTU_ID"), primary_key=True),
+        Column('SAMPLE_ID', String(64), ForeignKey("Sample.SAMPLE_ID"), primary_key=True),
+        Column("abs_val", Integer), 
+        Column("norm_val", Numeric)
+    )
+
+    meta.create_all(engine)
 
 
 def main():
+    # load config data
     config = load(CONFIG)
     
-    phylo_col = config["phylo_col"]
+    # read datas
     dataset, metadata = read_datas(config)
 
-    taxa_df = load_taxa(dataset, phylo_col)
+    # create dfs 
+    taxa_df = load_taxa(dataset, config["phylo_col"])
+    count_df = load_count(dataset, config["phylo_col"])
     sample_df = load_sample(metadata)
-    count_df = load_count(dataset, phylo_col)
 
+    # create database "sqlite"
+    if not exists(config["db"]):
+        init_database(config["db"])
+
+    # populate database 
     
 
 
 main()
+
+
