5f19b30d7c90cd609cff98835fa80322343dd7e0
tyrosine-rex <thomas.lahaie@etu.univ-lyon1.fr>
2023-01-30 13:46:38 +0100

    db_v2: sample df maker

--- src/db/setup_database_v2.py
+++ src/db/setup_database_v2.py
@@ -5,6 +5,15 @@ from pandas import read_csv
 CONFIG = "src/config.toml"
 
 
+def make_samples_df(metadata):
+    return metadata.set_index("SampleID") \
+        .rename_axis("SAMPLE_ID", axis="index") \
+        .rename_axis("WC", axis="columns") \
+        .apply(lambda x: x.str.lower()) \
+        .rename(columns={i: i.lower() for i in metadata.columns}) \
+        .astype("category")
+    
+
 def main():
     # load config
     config = load_toml(CONFIG)
@@ -18,6 +27,7 @@ def main():
     dataset = read_csv(dataset_path, sep="\t")
     metadata = read_csv(metadata_path, sep="\t")
 
+    samples = make_samples_df(metadata)
 
 
 if __name__ == "__main__":
