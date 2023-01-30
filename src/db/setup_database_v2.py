6d0dc2e990ef6443e1afbeda5c40caf8aa848d03
tyrosine-rex <thomas.lahaie@etu.univ-lyon1.fr>
2023-01-30 13:44:09 +0100

    db_v2: main: load config and csv

--- src/db/setup_database_v2.py
+++ src/db/setup_database_v2.py
@@ -1,11 +1,23 @@
 from toml import load as load_toml
+from pandas import read_csv
 
 
 CONFIG = "src/config.toml"
 
 
 def main():
+    # load config
     config = load_toml(CONFIG)
+    
+    # load path from config
+    db_path = config["db2"]
+    dataset_path = config["dataset"]
+    metadata_path = config["metadata"]
+
+    # load csv
+    dataset = read_csv(dataset_path, sep="\t")
+    metadata = read_csv(metadata_path, sep="\t")
+
 
 
 if __name__ == "__main__":
