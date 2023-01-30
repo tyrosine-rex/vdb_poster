29d9deb8cb5d8fa0679bc562bd64787560f07087
tyrosine-rex <thomas.lahaie@etu.univ-lyon1.fr>
2023-01-30 15:06:37 +0100

    setup_db_v2 replace setup_db and setup_db is OLD

---
+++ src/db/OLD_setup_database.py
@@ -0,0 +1,90 @@
+from os import remove
+from os.path import exists
+from toml import load
+from pandas import read_csv
+from sqlite3 import connect
+
+
+CONFIG = "src/config.toml"
+TABLES_CONFIG = "src/db/tables.toml"
+
+
+def load_taxa(df, taxa_info, taxa_colnames):
+    taxa = df[taxa_info].str \
+        .split(";", expand=True) \
+        .replace(".__|^ ", "", regex=True) \
+        .replace("\(.*\)", "", regex=True) \
+        .apply(lambda x:  x.str.lower()) \
+        .rename_axis("OTU_ID") \
+        .rename(columns={i: c for i, c in enumerate(taxa_colnames)})
+    return taxa
+
+
+def load_samples(df):
+    samples = df.rename_axis("SAMPLE_ID") \
+        .apply(lambda x: x.str.lower()) \
+        .rename(columns={i: i.lower() for i in df.columns})
+    return samples
+
+
+def load_counts(df, taxa_info):
+    counts_abs = df.drop(taxa_info, axis=1) \
+        .rename_axis("OTU_ID") \
+        .melt(ignore_index=False, var_name="SAMPLE_ID", value_name="abs_count")
+
+    counts_rel = df.drop(taxa_info, axis=1) \
+        .rename_axis("OTU_ID") \
+        .divide(df.sum()) \
+        .melt(ignore_index=False, var_name="SAMPLE_ID", value_name="rel_count")
+
+    counts = counts_abs.merge(counts_rel, on=["OTU_ID", "SAMPLE_ID"])
+    return counts
+
+
+def init_database(db_path, tables):
+    for sql in tables.values():
+        with connect(db_path) as conn:
+            conn.execute(sql)
+            conn.commit()
+
+
+def populate_database(db_path, taxa, counts, samples):
+    with connect(db_path) as conn: 
+        taxa.to_sql("Taxa", conn, if_exists="append") #append: keep constraint 
+        counts.to_sql("Counts", conn, if_exists="append")
+        samples.to_sql("Samples", conn, if_exists="append")
+
+
+def main():
+    # load config datas
+    config = load(CONFIG)
+    dataset_path = config["dataset"]
+    metadata_path = config["metadata"]
+    db_path = config["db"]
+
+    # load tables config datas
+    tables_config = load(TABLES_CONFIG)
+    taxa_info = tables_config["taxa_info"]
+    taxa_colnames = tables_config["taxa_colnames"]
+    tables = tables_config["tables"]
+
+    # read datas
+    dataset = read_csv(dataset_path, sep="\t", index_col=0)
+    metadata = read_csv(metadata_path, sep="\t", index_col=0)
+
+    # create df
+    taxa_df = load_taxa(dataset, taxa_info, taxa_colnames)
+    counts_df = load_counts(dataset, taxa_info)
+    samples_df = load_samples(metadata)
+
+    # create database "sqlite"
+    if exists(db_path):
+        remove(db_path)
+    init_database(db_path, tables)
+
+    # push df to database
+    populate_database(db_path, taxa=taxa_df, counts=counts_df, samples=samples_df)
+
+
+if __name__ == "__main__":
+    main()
--- src/db/setup_database.py
+++ src/db/setup_database.py
@@ -1,89 +1,126 @@
-from os import remove
-from os.path import exists
-from toml import load
+from toml import load as load_toml
 from pandas import read_csv
 from sqlite3 import connect
+from os import remove
+from os.path import exists
 
 
 CONFIG = "src/config.toml"
-TABLES_CONFIG = "src/db/tables.toml"
+TABLES = "src/db/tables.toml"
 
 
-def load_taxa(df, taxa_info, taxa_colnames):
-    taxa = df[taxa_info].str \
-        .split(";", expand=True) \
-        .replace(".__|^ ", "", regex=True) \
-        .replace("\(.*\)", "", regex=True) \
-        .apply(lambda x:  x.str.lower()) \
-        .rename_axis("OTU_ID") \
-        .rename(columns={i: c for i, c in enumerate(taxa_colnames)})
-    return taxa
-
-
-def load_samples(df):
-    samples = df.rename_axis("SAMPLE_ID") \
+def make_samples_df(metadata):
+    return metadata \
+        .set_index("SampleID") \
+        .rename_axis("SAMPLE_ID", axis="index") \
+        .rename_axis("WC", axis="columns") \
         .apply(lambda x: x.str.lower()) \
-        .rename(columns={i: i.lower() for i in df.columns})
-    return samples
+        .rename(columns={i: i.lower() for i in metadata.columns}) \
+        .astype("category") \
+        .copy()
+    
+
+def make_taxa_df(dataset):
+    taxa_colnames = ["reign", "phylum", "class", "_order", "family", "genus", "specie"]
+    return dataset \
+        .set_index("#OTU ID") \
+        .rename_axis("OTU_ID", axis="index") \
+        .get("ConsensusLineage") \
+        .str.lower() \
+        .str.split(";", expand=True) \
+        .replace(".__|\(.*\)", "", regex=True) \
+        .replace("^ | $", "", regex=True) \
+        .replace("", None, regex=True) \
+        .rename_axis("RANK", axis="columns") \
+        .rename(columns={i: c for i, c in enumerate(taxa_colnames)}) \
+        .astype("category") \
+        .copy()
 
 
-def load_counts(df, taxa_info):
-    counts_abs = df.drop(taxa_info, axis=1) \
-        .rename_axis("OTU_ID") \
-        .melt(ignore_index=False, var_name="SAMPLE_ID", value_name="abs_count")
+def make_counts_df(dataset):
+    return dataset \
+        .set_index("#OTU ID") \
+        .rename_axis("OTU_ID", axis="index") \
+        .rename_axis("SAMPLE_ID", axis="columns") \
+        .drop("ConsensusLineage", axis="columns") \
+        .astype("uint16") \
+        .copy()
 
-    counts_rel = df.drop(taxa_info, axis=1) \
-        .rename_axis("OTU_ID") \
-        .divide(df.sum()) \
-        .melt(ignore_index=False, var_name="SAMPLE_ID", value_name="rel_count")
 
-    counts = counts_abs.merge(counts_rel, on=["OTU_ID", "SAMPLE_ID"])
-    return counts
+def make_counts_rel_df(counts_abs):
+    return counts_abs \
+        .divide(counts_abs.sum(), axis="columns") \
+        .copy()
+
+
+def make_melted_counts(counts_abs, counts_rel):
+    melted_counts_abs = counts_abs \
+        .melt(ignore_index=False, var_name="SAMPLE_ID", value_name="abs_count") \
+        .reset_index() \
+        .set_index(["OTU_ID", "SAMPLE_ID"]) \
+        .rename_axis("COUNTS", axis="columns") \
+        .copy()
+
+    melted_counts_rel = counts_rel \
+        .melt(ignore_index=False, var_name="SAMPLE_ID", value_name="rel_count") \
+        .reset_index() \
+        .set_index(["OTU_ID", "SAMPLE_ID"]) \
+        .rename_axis("COUNTS", axis="columns") \
+        .copy()
+
+    return melted_counts_abs \
+        .merge(melted_counts_rel, on=["OTU_ID", "SAMPLE_ID"]) \
+        .copy()
 
 
 def init_database(db_path, tables):
+    if exists(db_path):
+        remove(db_path)
+
     for sql in tables.values():
         with connect(db_path) as conn:
             conn.execute(sql)
             conn.commit()
 
 
-def populate_database(db_path, taxa, counts, samples):
-    with connect(db_path) as conn: 
-        taxa.to_sql("Taxa", conn, if_exists="append") #append: keep constraint 
-        counts.to_sql("Counts", conn, if_exists="append")
-        samples.to_sql("Samples", conn, if_exists="append")
-
-
 def main():
-    # load config datas
-    config = load(CONFIG)
+    # load config
+    config = load_toml(CONFIG)
+    tables = load_toml(TABLES)
+
+    # load path from config
+    db_path = config["db"]
     dataset_path = config["dataset"]
     metadata_path = config["metadata"]
-    db_path = config["db"]
+    pkl_dir = config["pickle"]
 
-    # load tables config datas
-    tables_config = load(TABLES_CONFIG)
-    taxa_info = tables_config["taxa_info"]
-    taxa_colnames = tables_config["taxa_colnames"]
-    tables = tables_config["tables"]
+    # load csv
+    dataset = read_csv(dataset_path, sep="\t")
+    metadata = read_csv(metadata_path, sep="\t")
 
-    # read datas
-    dataset = read_csv(dataset_path, sep="\t", index_col=0)
-    metadata = read_csv(metadata_path, sep="\t", index_col=0)
+    # make df
+    samples = make_samples_df(metadata)
+    taxa = make_taxa_df(dataset)
+    counts_abs = make_counts_df(dataset)
+    counts_rel = make_counts_rel_df(counts_abs)
+    melted_counts = make_melted_counts(counts_abs, counts_rel)
 
-    # create df
-    taxa_df = load_taxa(dataset, taxa_info, taxa_colnames)
-    counts_df = load_counts(dataset, taxa_info)
-    samples_df = load_samples(metadata)
+    # export df to pickle format
+    samples.to_pickle(f"{pkl_dir}/samples.pkl")
+    taxa.to_pickle(f"{pkl_dir}/taxa.pkl")
+    counts_abs.to_pickle(f"{pkl_dir}/counts_abs.pkl")
+    counts_rel.to_pickle(f"{pkl_dir}/counts_rel.pkl")
+    melted_counts.to_pickle(f"{pkl_dir}/melted_counts.pkl")
 
-    # create database "sqlite"
-    if exists(db_path):
-        remove(db_path)
+    # init database
     init_database(db_path, tables)
 
-    # push df to database
-    populate_database(db_path, taxa=taxa_df, counts=counts_df, samples=samples_df)
+    # populate database
+    with connect(db_path) as conn: 
+        taxa.to_sql("Taxa", conn, if_exists="append") # append to keep constraints from 'init_database()'
+        melted_counts.to_sql("Counts", conn, if_exists="append")
+        samples.to_sql("Samples", conn, if_exists="append")
+        conn.commit()
 
 
 if __name__ == "__main__":
